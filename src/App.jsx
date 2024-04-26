c4f561efa527ec400aadbe394d28cacfb7db43d0
AKalamaghin <arteom.kalamaghin@isa.utm.md>
2024-04-26 09:47:04 +0300

    Basic app-page structure defined

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+import NewWspaceComponent from "./components/NewWspaceComponent";
+import WspaceListComponent from "./components/WspaceListComponent";
+import WspaceComponent from "./components/WspaceComponent";
+
+function App() {
+  localStorage.setItem("currentWs", "");
+
+  return (
+    <div
+      className="container"
+      style={{ maxWidth: "70vw", height: "100vh", backgroundColor: "white" }}
+    >
+      <div className="row" style={{ height: "100%" }}>
+        <div className="col-md-3 bg-dark text-white">
+          <div style={{ textAlign: "center", margin: "1vw 0" }}>
+            <p style={{ fontSize: "3vw" }}>FastPlot</p>
+          </div>
+
+          <div
+            style={{ height: "5px", background: "white", borderRadius: "5px" }}
+          ></div>
+
+          <div
+            className="container d-flex flex-column justify-content-center align-items-center"
+            style={{ margin: "1vw 0" }}
+          >
+            <p style={{ fontSize: "2vw" }}>Workspaces:</p>
+            <WspaceListComponent />
+            <NewWspaceComponent />
+          </div>
+        </div>
+
+        <div className="col-md-9 d-flex flex-column align-items-center">
+          <WspaceComponent />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
